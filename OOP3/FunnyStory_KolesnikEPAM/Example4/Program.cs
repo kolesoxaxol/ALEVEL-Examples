644c93b6ed539fb5c5cee6e24dc9694bb14914e3
Koliesnik Yevhenii <dp260793kev@yandex.ua>
2019-09-05 14:02:52 +0300

    Add several examples

---
+++ OOP3/FunnyStory_KolesnikEPAM/Example4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Example4
+{
+    // Base Class
+
+    public class User
+
+    {
+
+        public virtual void GetInfo()
+
+        {
+
+            Console.WriteLine("Base Class");
+
+        }
+
+    }
+
+    // Derived Class
+
+    public class SuperUser : User
+
+    {
+
+        public override void GetInfo()
+
+        {
+
+            Console.WriteLine("Derived Class");
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User obj = new SuperUser();
+            obj.GetInfo();
+            obj = new User();
+            obj.GetInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP3/FunnyStory_KolesnikEPAM/Example5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example5
+{
+    class Program
+    {
+        interface IPrinter
+        {
+            void Print();
+            int Scan();
+        }
+
+        class Printer: IPrinter
+        {
+            public void Revert()
+            {
+                Console.WriteLine("I revertImage");
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("I pint something");
+            }
+
+            public int Scan()
+            {
+                Console.WriteLine("I scan something");
+                Revert();
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IPrinter printer = new Printer();
+            printer.Print();
+            printer.Scan();
+            // Revert will be incapsulated 
+        }
+    }
+}
