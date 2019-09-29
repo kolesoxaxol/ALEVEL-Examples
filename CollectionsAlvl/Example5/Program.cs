5e713e8225103691b5820c4a97d4fa590ae1ef41
Koliesnik Yevhenii <dp260793kev@yandex.ua>
2019-09-29 20:19:59 +0300

    Add example 5

---
+++ CollectionsAlvl/Example5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example5
+{
+    class Program
+    {
+        private static IEnumerable<int> GetOddNumbers()
+        {
+            var previous = 0;
+            while (true)
+                if (++previous % 2 != 0)
+                    yield return previous;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var number in GetOddNumbers())
+                Console.WriteLine(number);
+        }
+    }
+}
---
+++ CollectionsAlvl/Example5/YieldDecompiler.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example5
+{
+    internal class Program1
+    {
+        private static void Main1()
+        {
+            IEnumerator<int> enumerator = null;
+            try
+            {
+                enumerator = GetOddNumbers().GetEnumerator();
+                while (enumerator.MoveNext())
+                    Console.WriteLine(enumerator.Current);
+            }
+            finally
+            {
+                if (enumerator != null)
+                    enumerator.Dispose();
+            }
+        }
+
+        [IteratorStateMachine(typeof(CompilerGeneratedYield))]
+        private static IEnumerable<int> GetOddNumbers()
+        {
+            return new CompilerGeneratedYield(-2);
+        }
+
+        [CompilerGenerated]
+        private sealed class CompilerGeneratedYield : IEnumerable<int>,
+            IEnumerable, IEnumerator<int>, IDisposable, IEnumerator
+        {
+            private readonly int _initialThreadId;
+            private int _current;
+            private int _previous;
+            private int _state;
+
+            [DebuggerHidden]
+            public CompilerGeneratedYield(int state)
+            {
+                _state = state;
+                _initialThreadId = Environment.CurrentManagedThreadId;
+            }
+
+            [DebuggerHidden]
+            IEnumerator<int> IEnumerable<int>.GetEnumerator()
+            {
+                CompilerGeneratedYield getOddNumbers;
+                if ((_state == -2) && (_initialThreadId == Environment.CurrentManagedThreadId))
+                {
+                    _state = 0;
+                    getOddNumbers = this;
+                }
+                else
+                {
+                    getOddNumbers = new CompilerGeneratedYield(0);
+                }
+
+                return getOddNumbers;
+            }
+
+            [DebuggerHidden]
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable<int>)this).GetEnumerator();
+            }
+
+            int IEnumerator<int>.Current
+            {
+                [DebuggerHidden]
+                get { return _current; }
+            }
+
+            object IEnumerator.Current
+            {
+                [DebuggerHidden]
+                get { return _current; }
+            }
+
+            [DebuggerHidden]
+            void IDisposable.Dispose() { }
+
+            bool IEnumerator.MoveNext()
+            {
+                switch (_state)
+                {
+                    case 0:
+                        _state = -1;
+                        _previous = 0;
+                        break;
+                    case 1:
+                        _state = -1;
+                        break;
+                    default:
+                        return false;
+                }
+
+                int num;
+                do
+                {
+                    num = _previous + 1;
+                    _previous = num;
+                } while (num % 2 == 0);
+
+                _current = _previous;
+                _state = 1;
+
+                return true;
+            }
+
+            [DebuggerHidden]
+            void IEnumerator.Reset()
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
