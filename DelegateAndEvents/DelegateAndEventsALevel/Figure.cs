bb98de2b86c164063e95f0174dfca20bd0c038b9
Koliesnik Yevhenii <dp260793kev@yandex.ua>
2019-10-06 17:59:26 +0300

    Add example 1

---
+++ DelegateAndEvents/DelegateAndEventsALevel/Figure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DelegateAndEventsALevel
+{
+    public static class Figure
+    {
+        // объявление статических методов в классе
+        // длина окружности
+        public static double GetLength(double r)
+        {
+            double length;
+            length = 2 * Math.PI * r;
+            return length;
+        }
+
+        // площадь круга
+        public static double GetArea(double r)
+        {
+            double area;
+            area = Math.PI * r * r;
+            return area;
+        }
+
+        // объем шара
+        public static double GetVolume(double r)
+        {
+            double volume;
+            volume = Math.PI * r * r * r * 4.0 / 3.0;
+            return volume;
+        }
+    }
+}
---
+++ DelegateAndEvents/DelegateAndEventsALevel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEventsALevel
+{
+    // объявление типа делегата CalcFigure
+    delegate double CalcFigure(double r);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, length, area, volume;
+
+
+            radius = 3.0;
+
+            // вызов делегата
+            // сконструировать делегат
+            CalcFigure CF = new CalcFigure(Figure.GetArea);
+            area = CF(radius);
+
+            CF = new CalcFigure(Figure.GetVolume);
+            volume = CF(radius);
+
+            CF = new CalcFigure(Figure.GetLength);
+            length = CF(radius); 
+
+            Console.WriteLine($"Площадь круга : {area}");
+            Console.WriteLine($"Объем шара : {volume}");
+            Console.WriteLine($"Длина окружности : {length}");
+
+            Console.ReadLine();
+        }
+    }
+}
