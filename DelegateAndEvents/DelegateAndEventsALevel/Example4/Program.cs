64920864649bce38a942dfd8d4d71ec71eb07716
Koliesnik Yevhenii <dp260793kev@yandex.ua>
2019-10-07 10:05:24 +0300

    Add example

---
+++ DelegateAndEvents/DelegateAndEventsALevel/Example4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Example4
+{
+    delegate Name UI(FamilyAndName obj);
+
+    public class Name
+    {
+        public string myName;
+    }
+
+    public class FamilyAndName : Name
+    {
+        public string Family;
+    }
+
+    public class UserInfo
+    {
+        public static Name UIName(Name obj)
+        {
+            obj.myName = "Имя пользователя: \"" + obj.myName + "\"";
+            return obj;
+        }
+
+        public static FamilyAndName UIFamilyName(FamilyAndName obj)
+        {
+            obj.Family = "Имя и фамилия: \"" + obj.myName + " " + obj.Family + "\"";
+            return obj;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Пример ковариантности
+            // Можно использовать метод, возвращаемым типом параметра которого 
+            // является производный класс
+            UI user1 = UserInfo.UIFamilyName;
+
+            // Пример контравариантности
+            // Можно использовать метод, аргументом которого является
+            // базовый класс
+            user1 = UserInfo.UIName;
+        
+            Console.ReadLine();
+        }
+    }
+}
