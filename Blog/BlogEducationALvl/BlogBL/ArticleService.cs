df0901caaee27e9ba356976434036686effe8e2c
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2019-12-20 12:02:34 +0200

    add blog project

---
+++ Blog/BlogEducationALvl/BlogBL/ArticleService.cs
@@ -0,0 +1,31 @@
+﻿using BlogBL.BLModels;
+using BlogDAL.Entities;
+using BlogDAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogBL
+{
+    // TODO: make it with generic services
+    public class ArticleService 
+    {
+        private readonly IGenericRepository<Article> repository;
+        public ArticleService()
+        { 
+            repository = new GenericRepository<Article>(); 
+        }
+        public ArticleBL GetById(int id)
+        {
+            var articleEntity = repository.FindById(id);
+            // TODO: Install mapper\
+            // TODO: finish mapping all fields
+            return new ArticleBL()
+            {
+                AuthorId = articleEntity.AuthorId
+            };
+        }
+    }
+}
---
+++ Blog/BlogEducationALvl/BlogEducationALvl/Controllers/ArticleController.cs
@@ -0,0 +1,95 @@
+﻿using BlogBL;
+using System.Web.Mvc;
+
+namespace BlogEducationALvl.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ArticleService _articleService;
+        public ArticleController()
+        {
+            _articleService = new ArticleService();
+        }
+
+        // GET: Article
+        public ActionResult Index()
+        {
+         
+            return View();
+        }
+
+        // GET: Article/Details/5
+        // TODO: add view for this action 
+        public ActionResult Details(int id)
+        {
+            _articleService.GetById(id);
+            return View();
+        }
+
+        // GET: Article/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Article/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Article/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Article/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Article/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Article/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
