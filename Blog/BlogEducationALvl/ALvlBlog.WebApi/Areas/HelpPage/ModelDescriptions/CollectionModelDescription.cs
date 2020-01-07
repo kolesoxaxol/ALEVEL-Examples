b7af8da50fa151811f0660e857fbdf22c8b45734
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2020-01-07 08:41:31 +0200

    Add web api

---
+++ Blog/BlogEducationALvl/ALvlBlog.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ALvlBlog.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Blog/BlogEducationALvl/ALvlBlog.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ALvlBlog.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
