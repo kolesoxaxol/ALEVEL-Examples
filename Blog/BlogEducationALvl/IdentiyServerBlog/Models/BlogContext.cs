07464c2f92a9e1d3045d07fbe0fb0093035db6ef
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2020-04-19 11:54:24 +0300

    Add api and indentity projects

---
+++ Blog/BlogEducationALvl/IdentiyServerBlog/Models/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace IdentiyServerBlog.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
